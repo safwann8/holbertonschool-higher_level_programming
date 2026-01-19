1900d44a9fd5bcf082d1460fa393d5042238d506
safwann8 <safoulefou83@gmail.com>
2026-01-19 15:53:24 +0100

    correction because i use 3 print

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,6 @@ def uppercase(str):
         char = ord(c)
         if char >= ord('a') and char <= ord('z'):
             upper_c = chr(char - 32)
-            print("{}".format(upper_c), end="")
         else:
             upper_c = c
             print("{}".format(upper_c), end="")
