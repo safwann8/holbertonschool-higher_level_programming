e8a549ca262550cd566d719e1ec824136fce0bc4
safwann8 <safoulefou83@gmail.com>
2026-01-19 04:56:33 +0000

    correction print "01" not "1"

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(00, 100):
     if number != 99:
-        print("{}".format(number), end=", ")
+        print("{:02d}".format(number), end=", ")
     else:
-        print("{}".format(number))
+        print("{:02d}".format(number))
