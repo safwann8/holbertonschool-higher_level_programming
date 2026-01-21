17129028864dcc4c0caf7e9a2287cb8fe32153ae
safwann8 <safoulefou83@gmail.com>
2026-01-21 16:41:18 +0100

    create and finishing the file 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for lst in matrix:
+        for i in range(len(lst)):
+            if i != 0:
+                print(" ", end="")
+            print("{:d}".format(lst[i]), end="")
+        print()
+        
