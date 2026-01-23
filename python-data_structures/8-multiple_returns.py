17c67c90ab98b56e905237f93dc1456412a20713
safwann8 <safoulefou83@gmail.com>
2026-01-23 22:26:55 +0100

    correction variable

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    len = len(sentence)
-    frst = sentence[0] if len(sentence) > 0 else None
-    return (len, frst)
+    lenght = len(sentence)
+    first = sentence[0] if len(sentence) > 0 else None
+    return (lenght, first)
