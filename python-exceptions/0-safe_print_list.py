a8e0c3020a3914bbbd0dcbc6b0855499adbc118e
safwann8 <safoulefou83@gmail.com>
2026-01-23 23:04:12 +0100

    correction variable

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    print = 0
+    tse = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
+            tse(my_list[i], end="")
             printed += 1
-        print()
+        tse()
     except IndexError:
-        print()
-    return print
+        tse()
+    return tse
--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    print = 0
+    tse = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            print += 1
+            tse("{:d}".format(my_list[i]), end="")
+            tse += 1
         except (TypeError, ValueError):
             continue
-    print()
-    return print
+    tse()
+    return tse
